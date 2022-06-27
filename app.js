45517f76b96c259a95571bd69cc37ce31f57fb47
Irvan Saharudin <irvansaharudin@gmail.com>
2022-06-27 22:42:38 +0700

    clean bug on duplicate 0.1

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ const clearbtn = document.querySelector('.clear');
 const target = 24;
 let finalResults = [];
 let starterValue = [];
-let loading = false;
 
 formInput.addEventListener('submit', (e)=>{
     e.preventDefault();
     starterValue = [firstNumber.value, secondNumber.value, thirdNumber.value,fourthNumber.value];
-    
-    loading = true;
-    finalResults =[];
-    getGameResults('', 0);
-    loading = false;
-    showSolution();
+    finalResults = [];
+    getGameResults('', 0)
+    showPossibleResults();
 })
 
 
@@ -30,64 +26,95 @@ function getGameResults(curExprs, curNumb){
         if(!isSimiliar.length){
             if(curNumb == starterValue.length){
                 finalResults.push(curExprs)
-                return
             }
         }
     }
     const operator = ['+', '-', '*', '/'];
     // looping for the current expression
     for(let escNumb = curNumb; escNumb < starterValue.length; escNumb++ ){
-        // looping for choosing the operator
+        // looping for choosing the next value
        if(curNumb == 0){
         getGameResults(`${starterValue[escNumb]}`, curNumb + 1)
        }else{
         for(let opr = 0; opr < starterValue.length; opr++){
-            // make sure there is no duplicate number in the current expression with the walue will assigned
-                getGameResults(`${curExprs} + ${starterValue[opr]}`, curNumb + 1);
-                getGameResults(`${curExprs} - ${starterValue[opr]}`, curNumb + 1);
-                getGameResults(`${curExprs} * ${starterValue[opr]}`, curNumb + 1);
-                getGameResults(`${curExprs} / ${starterValue[opr]}`, curNumb + 1);                
+            // check if there is a same value on the starter value
+            const isDuplicate = searchSimiliarValue(starterValue);
+            if(isDuplicate){
+                const numberOfDuplicate = searchDuplicateOnList(starterValue[opr]);
+                const numberOfDuplicateonExpression = searchDuplicateOnExpression(starterValue[opr], curExprs);
+                // compare if the number of value that have duplicate on list is more than already on current expressions  
+                if(numberOfDuplicateonExpression.length >= numberOfDuplicate.length){
+                    return;
+                }else{
+                    getGameResults(`${curExprs} + ${starterValue[opr]}`, curNumb + 1);
+                    getGameResults(`${curExprs} - ${starterValue[opr]}`, curNumb + 1);
+                    getGameResults(`${curExprs} * ${starterValue[opr]}`, curNumb + 1);
+                    getGameResults(`${curExprs} / ${starterValue[opr]}`, curNumb + 1);
+                }
+            }else{
+                // make sure there is no duplicate number in the current expression with the walue will assigned
+                if(!curExprs.includes(starterValue[opr])){
+                    getGameResults(`${curExprs} + ${starterValue[opr]}`, curNumb + 1);
+                    getGameResults(`${curExprs} - ${starterValue[opr]}`, curNumb + 1);
+                    getGameResults(`${curExprs} * ${starterValue[opr]}`, curNumb + 1);
+                    getGameResults(`${curExprs} / ${starterValue[opr]}`, curNumb + 1);
+                }
             }
+        }
        }
     }
+    
 }
 
-// function searchSimiliarValue(arry){
-//     let resultToReturn = false;
-//     for(let i = 0; i < arry.length; i++){
-//         for(let j = 0; j < arry.length; j++){
-//             if( i !== j){
-//                 if( arry[i] == arry[j]){
-//                     return resultToReturn = true;
-//                 }
-//             }
-//         }
-//     }
-//     return resultToReturn
-// }
 
-function showSolution(){
+function searchDuplicateOnList(num){
+    return starterValue.filter(item =>{
+        return item == num;
+    })
+}
+function searchDuplicateOnExpression(target, origin){
+    let results =[];
+    for(let i = 0; i < origin.length; i++){
+        if(target == origin[i]){
+            results.push(origin[i])
+        }
+    }
+    return results;
+}
+
+function searchSimiliarValue(arry){
+    let resultToReturn = false;
+    for(let i = 0; i < arry.length; i++){
+        for(let j = 0; j < arry.length; j++){
+            if( i !== j){
+                if( arry[i] == arry[j]){
+                    return resultToReturn = true;
+                }
+            }
+        }
+    }
+    return resultToReturn
+}
+
+function showPossibleResults(){
     const answerContainer = document.querySelector('.answer-list');
-    if(!loading){
-        if(finalResults.length > 0){
-            answerContainer.innerHTML= '';
-            finalResults.forEach(res =>{
-                const answer = document.createElement('li');
-                answer.classList.add('answer');
-                answer.innerHTML = res;
-                answerContainer.append(answer)
-            })
-        }else{
-            answerContainer.innerHTML= '';
+    if(finalResults.length > 0){
+        answerContainer.innerHTML= '';
+        finalResults.forEach(res =>{
             const answer = document.createElement('li');
             answer.classList.add('answer');
-            answer.innerHTML = 'Sorry no solution is found';
+            answer.innerHTML = res;
             answerContainer.append(answer)
-        }
+        })
     }else{
-        answerContainer.innerHTML = 'loading';
+        answerContainer.innerHTML= '';
+        const answer = document.createElement('li');
+        answer.classList.add('answer');
+        answer.innerHTML = 'Sorry no solution is found';
+        answerContainer.append(answer)
     }
-    }
+
+}
 
 clearbtn.addEventListener('click', ()=>{
     const answerContainer = document.querySelector('.answer-list');
